a5216fea285f2cf2846ae6027311c3c68c330956
Francisco Petri <franciscopetri@gmail.com>
2023-07-04 16:46:37 -0300

    Componente Register y Login, Auth Local agregada también

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <>CREANDO LA PAGINA</>;
+  return (
+    <main className="App">
+      <Routes>
+        <Route exact path="/" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
