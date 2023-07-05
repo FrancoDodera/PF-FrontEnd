998a856cb1cbfbf02807654ad5612282df9b8a2e
FrancoDodera <francododera@hotmail.com>
2023-07-05 17:18:53 -0300

    Estilos
    estilos no terminados

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,16 +1,20 @@
 import style from "./Card.module.css";
+import { Link } from "react-router-dom";
 
 const Card = (props) => {
   return (
-    <div>
-      <p>{props.id}</p>
-      <h3>{props.name} </h3>
-      <p>{props.description}</p>
-      <p>{props.category}</p>
-      <img src={props.image} alt="" />
-      <p>{props.price}</p>
-      <p>{props.age}</p>
-    </div>
+    <>
+      <Link className={style.Link} to={`/detail/${props.id}`}>
+        <div className={style.container}>
+          <img className={style.imageCointainer} src={props.image} alt="" />
+          <div>
+            <h3>{props.name} </h3>
+            <p className={style.p}>Price:{props.price}</p>
+            <p>{props.description}</p>
+          </div>
+        </div>
+      </Link>
+    </>
   );
 };
 
