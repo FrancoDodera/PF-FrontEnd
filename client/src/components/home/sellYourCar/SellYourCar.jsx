9d1f46a87fafc3a3ba7be62f43af97b55e2ef4e7
monocuco01 <119557633+monocuco01@users.noreply.github.com>
2023-07-05 17:06:42 -0500

    implemntacion de cards/ recomends cars y del apartado de c

---
+++ client/src/components/home/sellYourCar/SellYourCar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./sellYourCar.css";
+const SellYourCar = () => {
+  return (
+    <div className="container_sellYourCar">
+      <h1>Sell your car</h1>
+      <p>
+        Get instant offer • List for free 2 easy ways to sell your car today!
+      </p>
+      <button>Get started</button>
+    </div>
+  );
+};
+
+export default SellYourCar;
