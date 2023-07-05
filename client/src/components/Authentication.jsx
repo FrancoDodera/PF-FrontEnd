ff862f201318d361951e7fb396a4bb157cf292af
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-05 14:32:07 -0400

    arreglo de autenticacion

--- client/src/components/Authentication.jsx
+++ client/src/components/Authentication.jsx
@@ -1,16 +1,23 @@
 import GoogleLogin from 'react-google-login'
 import {gapi} from 'gapi-script'
 import { useEffect } from 'react'
+import {NavLink,useNavigate} from 'react-router-dom'
+
 
 const Authentication=()=>{
+    const navigate=useNavigate();
     const clientID="193073335991-hclacbarkbgi6vgh4ntd2ig24cnsfvpb.apps.googleusercontent.com"
     const handlerSuccess=(res)=>{
         //logica aca
         console.log({
-            Name:res.profileObj.name,
-            LastName:res.profileObj.familyName,
-            Email:res.profileObj.email
+            name:res.profileObj.name,
+            lastName:res.profileObj.familyName,
+            Email:res.profileObj.email,
+            user:res.profileObj.email,
+            password:res.tokenObj.login_hint,
+            dni:null
         })
+        navigate('/home')
     }
     const handlerError=(res)=>{
         //logica aca
