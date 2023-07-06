c39d31e74601a4c3f3d3e5af762bf8e9fc86153e
FrancoDodera <francododera@hotmail.com>
2023-07-06 12:38:30 -0300

    carsforsale
    diseño y agregados

--- client/src/components/Carsforsale/Carsforsale.jsx
+++ client/src/components/Carsforsale/Carsforsale.jsx
@@ -1,10 +1,16 @@
 import NavBar from "../navbar/NavBar";
 import CardsContainer from "../Cards/Cards";
 import Filters from "../filters/Filters";
+import SearchComponent from "../SearchBar/SearchBar";
+import style from "./Carsforsale.module.css";
 const Carsforsale = (props) => {
   return (
     <div>
       <NavBar />
+      <div className={style.searchContainer}>
+        <h2>New or used vehicles for sale</h2>
+        <SearchComponent />
+      </div>
       <Filters />
       <CardsContainer />
     </div>
