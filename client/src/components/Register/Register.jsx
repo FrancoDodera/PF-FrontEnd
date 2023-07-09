60c709080a63c4071535213366fb71311f2e1813
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-09 17:49:47 -0400

    arreglos

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,132 +1,193 @@
 import { useRef, useState, useEffect } from "react";
-import { NavLink,useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import axios from "axios";
 import styles from "./Register.module.css";
 import Swal from "sweetalert2";
 const USER_REGEX = /^[A-Za-z][A-Za-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const MAIL_REGEX =/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const MAIL_REGEX =
+  /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 const REGISTER_URL = "/user/addUser";
 
 const Register = () => {
-  const navigate=useNavigate()
-  const nameRef = useRef();
-  const lastNameRef = useRef();
-  const emailRef = useRef();
-  const userRef = useRef();
-  const errRef = useRef();
-
-  const [name, setName] = useState("");
-  const [validName, setValidName] = useState(false);
-
-  const [lastName, setLastName] = useState("");
-  const [validLastName, setValidLastName] = useState(false);
-
-  const [email, setEmail] = useState("");
-  const [validEmail, setValidEmail] = useState(false);
-
-  const [user, setUser] = useState("");
-  const [validUser, setValidUser] = useState(false);
-
-  const [pwd, setPwd] = useState("");
-  const [validPwd, setValidPwd] = useState(false);
-
-  const [matchPwd, setMatchPwd] = useState("");
-  const [validMatch, setValidMatch] = useState(false);
-
-  const [errMsg, setErrMsg] = useState("");
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    user: "",
+    password: "",
+    confirmPassword: "",
+    dni: "",
+  });
+  const [error, setError] = useState({
+    name: "",
+    lastName: "",
+    email: "",
+    user: "",
+    password: "",
+    confirmPassword: "",
+    dni: "",
+  });
   const [success, setSuccess] = useState(false);
+  const handlerForm = (event) => {
+    //validaciones
+    if (event.target.name == "email") {
+      if (!MAIL_REGEX.test(event.target.value)) {
+        setError({
+          ...error,
+          [event.target.name]: "email invalid",
+        });
+        setSuccess(false);
+      } else {
+        setError({
+          ...error,
+          [event.target.name]: "",
+        });
+      }
+    } else if (event.target.name == "user") {
+      if (!USER_REGEX.test(event.target.value)) {
+        setError({
+          ...error,
+          [event.target.name]: "user invalid",
+        });
+        setSuccess(false);
+      } else {
+        setError({
+          ...error,
+          [event.target.name]: "",
+        });
+      }
+    } else if (event.target.name == "password") {
+      if (!PWD_REGEX.test(event.target.value)) {
+        setError({
+          ...error,
+          [event.target.name]: "password invalid",
+        });
+        setSuccess(false);
+      } else {
+        setError({
+          ...error,
+          [event.target.name]: "",
+        });
+      }
+    } else if (event.target.name == "confirmPassword") {
+      if (form.password !== event.target.value) {
+        setError({
+          ...error,
+          [event.target.name]: "Passwords do not match",
+        });
+        setSuccess(false);
+      } else {
+        setError({
+          ...error,
+          [event.target.name]: "",
+        });
+      }
+    } else if (event.target.name == "name") {
+      if (event.target.value == "") {
+        setError({
+          ...error,
+          [event.target.name]: "Name required",
+        });
+        setSuccess(false);
+      } else {
+        setError({
+          ...error,
+          [event.target.name]: "",
+        });
+      }
+    } else if (event.target.name == "lastName") {
+      if (event.target.value == "") {
+        setError({
+          ...error,
+          [event.target.name]: "Last Name required",
+        });
+        setSuccess(false);
+      } else {
+        setError({
+          ...error,
+          [event.target.name]: "",
+        });
+      }
+    }
 
-  useEffect(() => {
-    nameRef.current.focus();
-  }, []);
-
-  useEffect(() => {
-    setValidName(USER_REGEX.test(name));
-  }, [name]);
-
-  useEffect(() => {
-    setValidLastName(USER_REGEX.test(lastName));
-  }, [lastName]);
-
-  useEffect(() => {
-    setValidEmail(MAIL_REGEX.test(email));
-  }, [email]);
-
-  useEffect(() => {
-    setValidUser(USER_REGEX.test(user));
-  }, [user]);
-
-  useEffect(() => {
-    setValidPwd(PWD_REGEX.test(pwd));
-    setValidMatch(pwd === matchPwd);
-  }, [pwd, matchPwd]);
-
-  useEffect(() => {
-    setErrMsg("");
-  }, [name, lastName, email, user, pwd, matchPwd]);
-
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (
+      form.name == "" ||
+      form.lastName == "" ||
+      form.email == "" ||
+      form.user == "" ||
+      form.password == "" ||
+      form.confirmPassword == ""
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "Missing Data",
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 400,
+      });
+    } else if (
+      error.name !== "" ||
+      error.lastName !== "" ||
+      error.email !== "" ||
+      error.user !== "" ||
+      error.password !== "" ||
+      error.confirmPassword !== ""
+    ) {
+      Swal.fire({
+        icon: "error",
+        title: "Invalid Data",
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 400,
+      });
+    } else {
+      try {
+        const body = {
+          name: form.name,
+          lastName: form.lastName,
+          email: form.email,
+          user: form.user,
+          password: form.password,
+          dni: null,
+        };
 
-    try {
-      const body = {
-        name: name,
-        lastName: lastName,
-        email: email,
-        user: user,
-        password: pwd,
-        dni: null,
-      };
-
-      const { data } = await axios.post(REGISTER_URL, body);
-      if (data.acces) {
-        Swal.fire({
-          icon: "success",
-          title: "User created",
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 500,
-        });
-        setSuccess(true);
-        setName("");
-        setLastName("");
-        setEmail("");
-        setUser("");
-        setPwd("");
-        setMatchPwd("");
-        navigate('/login')
-      }else{
-        Swal.fire({
-          icon: "error",
-          title: "Error",
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 500,
-        });
+        const { data } = await axios.post(REGISTER_URL, body);
+        if (data.acces) {
+          Swal.fire({
+            icon: "success",
+            title: "User created",
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 500,
+          });
+          navigate("/login");
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "Error",
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 500,
+          });
+        }
+      } catch (err) {
+        alert(err);
       }
-    } catch (err) {
-      if (!err?.response) {
-        setErrMsg("No Server Response");
-      } else if (err.response?.status === 409) {
-        setErrMsg("Username Taken");
-      } else {
-        setErrMsg("Registration failed");
-      }
-      errRef.current.focus();
     }
   };
 
   return (
     <div className={styles.container}>
       <section className={styles.container2}>
-        <p
-          ref={errRef}
-          className={errMsg ? styles.errmsg : styles.offscreen}
-          aria-live="assertive"
-        >
-          {errMsg}
-        </p>
         <h1>Register</h1>
         <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.formcito3}>
@@ -136,80 +197,102 @@ const Register = () => {
                 <input
                   type="text"
                   id="name"
-                  ref={nameRef}
+                  name="name"
                   autoComplete="off"
-                  onChange={(e) => setName(e.target.value)}
-                  value={name}
+                  onChange={handlerForm}
+                  value={form.name}
                 />
               </div>
+              {error.name ? (
+                <p className={styles.errorMsg}>{error.name}</p>
+              ) : (
+                ""
+              )}
+
               <div className={styles.inputContainerName}>
                 <label htmlFor="lastName">Last Name:</label>
                 <input
                   type="text"
                   id="lastName"
-                  ref={lastNameRef}
+                  name="lastName"
                   autoComplete="off"
-                  onChange={(e) => setLastName(e.target.value)}
-                  value={lastName}
+                  onChange={handlerForm}
+                  value={form.lastName}
                 />
               </div>
+              {error.lastName ? (
+                <p className={styles.errorMsg}>{error.lastName}</p>
+              ) : (
+                ""
+              )}
               <div className={styles.inputContainerName}>
                 <label htmlFor="email">Email:</label>
                 <input
                   type="email"
                   id="email"
-                  ref={emailRef}
+                  name="email"
                   autoComplete="off"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
+                  onChange={handlerForm}
+                  value={form.email}
                 />
               </div>
+              {error.email ? (
+                <p className={styles.errorMsg}>{error.email}</p>
+              ) : (
+                ""
+              )}
             </div>
             <div className={styles.formcito}>
               <div className={styles.inputContainerName}>
-                <label htmlFor="username">Username:</label>
+                <label htmlFor="user">Username:</label>
                 <input
                   type="text"
-                  id="username"
-                  ref={userRef}
+                  id="user"
+                  name="user"
                   autoComplete="off"
-                  onChange={(e) => setUser(e.target.value)}
-                  value={user}
+                  onChange={handlerForm}
+                  value={form.user}
                 />
               </div>
+              {error.user ? (
+                <p className={styles.errorMsg}>{error.user}</p>
+              ) : (
+                ""
+              )}
               <div className={styles.inputContainerName}>
                 <label htmlFor="password">Password:</label>
                 <input
                   type="password"
                   id="password"
-                  onChange={(e) => setPwd(e.target.value)}
-                  value={pwd}
+                  name="password"
+                  onChange={handlerForm}
+                  value={form.password}
                 />
               </div>
+              {error.password ? (
+                <p className={styles.errorMsg}>{error.password}</p>
+              ) : (
+                ""
+              )}
               <div className={styles.inputContainerName}>
-                <label htmlFor="confirm_pwd">Confirm Password:</label>
+                <label htmlFor="confirmPassword">Confirm Password:</label>
                 <input
                   type="password"
-                  id="confirm_pwd"
-                  onChange={(e) => setMatchPwd(e.target.value)}
-                  value={matchPwd}
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  onChange={handlerForm}
+                  value={form.confirmPassword}
                 />
               </div>
+              {error.confirmPassword ? (
+                <p className={styles.errorMsg}>{error.confirmPassword}</p>
+              ) : (
+                ""
+              )}
             </div>
           </div>
           <div className={styles.botonnn}>
-            <button
-              disabled={
-                !validName ||
-                !validLastName ||
-                !validEmail ||
-                !validUser ||
-                !validPwd ||
-                !validMatch
-              }
-            >
-              Register
-            </button>
+            <button>Register</button>
           </div>
         </form>
         <p className={styles.signInLink}>
