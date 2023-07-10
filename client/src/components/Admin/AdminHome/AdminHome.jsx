468e72124c54c6e890f091d5bdc5b2353bce712e
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-10 18:47:51 -0400

    arreglos rutas admin

--- client/src/components/Admin/AdminHome/AdminHome.jsx
+++ client/src/components/Admin/AdminHome/AdminHome.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 const AdminHome = () => {
   return (
-    <div className={style.adminContainer}>
-        <h1>admin</h1>
+    <div>
+        <h1>Admin Home</h1>
     </div>
   );
 };
---
+++ client/src/components/Admin/Sale/Sale.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sale = () => {
+  return (
+    <div>
+        <h1>Sale</h1>
+    </div>
+  );
+};
+
+export default Sale;
