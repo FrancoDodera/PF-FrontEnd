1d514b388e18ccc56916be1fbbe6063362d7a3f6
Francisco Petri <franciscopetri@gmail.com>
2023-07-11 16:46:32 -0300

    Update User.jsx

--- client/src/components/Admin/User/User.jsx
+++ client/src/components/Admin/User/User.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "../../../redux/actions";
 import { useNavigate } from "react-router-dom";
@@ -11,31 +10,35 @@ const User = () => {
     dispatch(getAllUsers());
   }, []);
   const navigate = useNavigate();
+
   return (
     <div className="w-full h-[110vh] bg-[#0a192f] text-gray-300">
-          <button
+      <button
         className="btn-secondary flex justify-between p-8 text-gray-300"
         onClick={() => navigate("/admin")}
       >
         Go Home
       </button>
-      <h1 className="text-3xl font-bold flex justify-between p-8 text-gray-300">User</h1>
-      {/* You can open the modal using ID.showModal() method */}
+      <h1 className="text-3xl font-bold flex justify-between p-8 text-gray-300">
+        User
+      </h1>
       <button className="btn" onClick={() => window.my_modal_4.showModal()}>
         open modal
       </button>
-      <dialog id="my_modal_4" className="modal" >
+      <dialog id="my_modal_4" className="modal">
         <form method="dialog" className="modal-box w-11/12 max-w-5xl h-auto">
-        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+          <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            ✕
+          </button>
           <h3 className="font-bold text-lg">Hello!</h3>
           <p className="py-4">Click the button below to close</p>
-          
+
           <div className="modal-action">
             <button className="btn">save</button>
           </div>
         </form>
       </dialog>
-      <table className="className="w-full h-[110vh] bg-[#0a192f] text-gray-300"">
+      <table className="w-full h-[110vh] bg-[#0a192f] text-gray-300">
         <thead>
           <tr>
             <th></th>
