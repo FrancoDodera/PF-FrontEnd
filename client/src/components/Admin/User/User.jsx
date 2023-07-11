81d2b7d697018b89009000eca2f71ced9fb2d086
Francisco Petri <franciscopetri@gmail.com>
2023-07-11 16:41:20 -0300

    Estilos dashboard

--- client/src/components/Admin/User/User.jsx
+++ client/src/components/Admin/User/User.jsx
@@ -1,51 +1,60 @@
 import React from "react";
-import { useEffect,useState } from "react";
-import {useDispatch,useSelector} from 'react-redux';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "../../../redux/actions";
+import { useNavigate } from "react-router-dom";
 
 const User = () => {
-  const dispatch=useDispatch();
-  const users=useSelector((state)=>state.allUsers)
-  useEffect(()=>{
+  const dispatch = useDispatch();
+  const users = useSelector((state) => state.allUsers);
+  useEffect(() => {
     dispatch(getAllUsers());
-  },[])
+  }, []);
+  const navigate = useNavigate();
   return (
-    <div>
-        <h1>User</h1>
-        <table className="table">
-          <thead>
-            <tr>
-              <th></th>
-              <th>UserName</th>
-              <th>Email</th>
-              <th>Name</th>
-              <th>Last Name</th>
-              <th>Status</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              users?.map((user,index)=>{
-                return(
-                  <tr key={user._id}>
-                    <td>{index+1}</td>
-                    <td>{user.user}</td>
-                    <td>{user.email}</td>
-                    <td>{user.name}</td>
-                    <td>{user.lastName}</td>
-                    <td>{user.status?'Available':'Disabled'}</td>
-                    <td>
-                      <div className="btn-group">
-                        <button className="btn btn-success">Update</button><button className="btn btn-error">Delete</button>
-                      </div>
-                    </td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
+    <div className="w-full h-[110vh] bg-[#0a192f] text-gray-300">
+      <button
+        className="btn-secondary flex justify-between p-8 text-gray-300"
+        onClick={() => navigate("/admin")}
+      >
+        Go Home
+      </button>
+      <h1 className="text-3xl font-bold flex justify-between p-8 text-gray-300">
+        User
+      </h1>
+      <table className="w-full h-[110vh] bg-[#0a192f] text-gray-300">
+        <thead>
+          <tr>
+            <th></th>
+            <th>UserName</th>
+            <th>Email</th>
+            <th>Name</th>
+            <th>Last Name</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((user, index) => {
+            return (
+              <tr key={user._id}>
+                <td>{index + 1}</td>
+                <td>{user.user}</td>
+                <td>{user.email}</td>
+                <td>{user.name}</td>
+                <td>{user.lastName}</td>
+                <td>{user.status ? "Available" : "Disabled"}</td>
+                <td>
+                  <div className="btn-group">
+                    <button className="btn btn-success">Update</button>
+                    <button className="btn btn-error">Delete</button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
