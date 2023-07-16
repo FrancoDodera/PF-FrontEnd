1d4742e8f5a3b4a062537057fc4dd552e787815d
Francisco Petri <franciscopetri@gmail.com>
2023-07-16 18:52:59 -0300

    Purchases

--- client/src/components/Detail-Carrito/DetailCart.jsx
+++ client/src/components/Detail-Carrito/DetailCart.jsx
@@ -1,76 +1,78 @@
-import { useState } from "react"
-import NavBar from "../navbar/NavBar"
-import Card from "./CardDetailCart"
-import style from "./DetailCart.module.css"
-import axios from"axios"
-const DetailCart=()=>{
-    const[loading,setLoading]= useState(false)
-    const Cars= localStorage.getItem('cartItems')
-    let cars = JSON.parse(Cars)
-        console.log(cars)
-    const total='TOTAL'
-    let Pay=0
-    for (let i = 0; i < cars.length; i++) {
-        Pay=Pay + cars[i].totalPrice
-        
-    }
-    const redirect =async()=>{
-       setLoading(true)
-       const data=await axios.post('https://pf-back.fly.dev/checkout',cars )
-       console.log(data.data.response.response.init_point)
-        window.location.href=data.data.response.response.init_point
-        
-    }
-    
+import { useState } from "react";
+import NavBar from "../navbar/NavBar";
+import Card from "./CardDetailCart";
+import axios from "axios";
 
-    if (loading===true) {
-            return(
-                <div class={style.overlay}>
-                <div class={style.loader}></div>
-                <h1>redirecting</h1>
-             </div>
-            )
-        }
+const DetailCart = () => {
+  const [loading, setLoading] = useState(false);
+  const Cars = localStorage.getItem("cartItems");
+  let cars = JSON.parse(Cars);
+  console.log(cars);
+  const total = "TOTAL";
+  let Pay = 0;
+  for (let i = 0; i < cars.length; i++) {
+    Pay = Pay + cars[i].totalPrice;
+  }
+  const redirect = async () => {
+    setLoading(true);
+    const data = await axios.post("https://pf-back.fly.dev/checkout", cars);
+    console.log(data.data.response.response.init_point);
+    window.location.href = data.data.response.response.init_point;
+  };
 
-    return(
-        <>
-        <NavBar/>
-        <div className=''>
-            <div className="relative left-96 max-w-3xl z-0 ">
-                {cars.map((car)=><Card 
-                name={car?.name}
-                price={car?.price}
-                amount={car.amount}
-                totalPrice={car.totalPrice}
-                image={car?.image}
-                />) }
-                {loading && (
-                    <div className="overlay">
-                        <div className="loader"></div>
-                    </div>
-                )}
+  if (loading === true) {
+    return (
+      <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50 bg-black bg-opacity-50">
+        <div className="border-4 border-white rounded-full h-16 w-16 animate-spin"></div>
+        <h1 className="ml-2 text-white">Redirecting</h1>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <NavBar />
+      <div className="flex">
+        <div className="relative left-96 max-w-3xl z-0">
+          {cars.map((car) => (
+            <Card
+              key={car.id}
+              name={car?.name}
+              price={car?.price}
+              amount={car.amount}
+              totalPrice={car.totalPrice}
+              image={car?.image}
+            />
+          ))}
+          {loading && (
+            <div className="overlay">
+              <div className="loader"></div>
             </div>
-            
-                <div className=" border-4 absolute top-60 right-40 w-96 h-3/6 rounded-xl">
-                    <h1 className="mx-36 mt-10 text-3xl">cars:</h1>
-                    <div className="   h-60 w-80 border-2 ml-7 rounded-xl">
-                            {cars.map((car)=>{return(
-                                <div className={style.calculator}>
-                                    <h6 >$ {car.totalPrice}</h6>
-                                </div>
-                                )})}
-                    </div>
-                    <h1 className={` inline-block ${style.to}`}>{total}</h1>
-                    
-                    <h1 className={`text-right ${style.total}`}>$ {Pay}</h1>
-
-                    <button className={style.but} onClick={redirect}>RESERVE</button>
-                    
-                </div>
-                
+          )}
         </div>
-        </>
-    )
-}
 
-export default DetailCart
+        <div className="border-4 absolute top-60 right-40 w-96 h-3/6 rounded-xl">
+          <h1 className="mx-36 mt-10 text-3xl">Cars:</h1>
+          <div className="h-60 w-80 border-2 ml-7 rounded-xl">
+            {cars.map((car) => (
+              <div className="calculator" key={car.id}>
+                <h6 className="inline">{`$ ${car.totalPrice}`}</h6>
+              </div>
+            ))}
+          </div>
+          <h1 className="inline-block ml-7 mt-1">{total}</h1>
+          <h1 className="text-right text-2xl mr-10 mt-1">${Pay}</h1>
+
+          <button
+            className="p-2 px-4 bg-gray-200 border-2 border-gray-200 rounded text-black text-lg cursor-pointer transition duration-300 hover:bg-teal-400 active:bg-purple-600 absolute bottom-10 left-1/2 transform -translate-x-1/2"
+            onClick={redirect}
+          >
+            RESERVE
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DetailCart;
