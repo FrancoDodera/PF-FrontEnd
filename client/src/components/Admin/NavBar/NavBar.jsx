e04311cf0827fd63292cb68f4be701dad7509a81
monocuco01 <119557633+monocuco01@users.noreply.github.com>
2023-07-16 15:33:02 -0500

    arreglo adminhome

--- client/src/components/Admin/NavBar/NavBar.jsx
+++ client/src/components/Admin/NavBar/NavBar.jsx
@@ -10,59 +10,65 @@ const NavBar = () => {
     navigate("/login");
   };
   return (
-    
-      <div className="containerSlite bg-[#597091]">
-        <aside>
-          <div className="top">
-            <div className="logo">
-              <img src={logo} alt="logo" />
-            </div>
+    <div className="containerSlite bg-[#597091]">
+      <aside>
+        <div className="top">
+          <div className="logo">
+            <img src={logo} alt="logo" />
           </div>
-          <div className="sidebar">
-            <div className="home btn btn-ghost normal-case text-xl">
-              <span className="material-symbols-sharp">home</span>
+        </div>
+        <div className="sidebar">
+          <div
+            onClick={() => navigate("/admin")}
+            className="home btn btn-ghost normal-case text-xl"
+          >
+            <span className="material-symbols-sharp">home</span>
 
-              <a onClick={() => navigate("/admin")}>Home</a>
-            </div>
-            <div className="brand btn btn-ghost normal-case text-xl">
-              <span class="material-symbols-sharp">bolt</span>
-              <a className="" onClick={() => navigate("/admin/brand")}>
-                Brand
-              </a>
-            </div>
-            <div className="category btn btn-ghost normal-case text-xl">
-              <span class="material-symbols-sharp">category</span>{" "}
-              <a className="" onClick={() => navigate("/admin/category")}>
-                Category
-              </a>
-            </div>
-            <div className="car btn btn-ghost normal-case text-xl">
-              <span class="material-symbols-sharp">directions_car</span>{" "}
-              <a className="" onClick={() => navigate("/admin/car")}>
-                Car
-              </a>
-            </div>
-            <div className="sales btn btn-ghost normal-case text-xl">
-              <span class="material-symbols-sharp">sell</span>{" "}
-              <a className="" onClick={() => navigate("/admin/sale")}>
-                Sale
-              </a>
-            </div>
-            <div className="user btn btn-ghost normal-case text-xl">
-              <span class="material-symbols-sharp">person</span>{" "}
-              <a className="" onClick={() => navigate("/admin/user")}>
-                User
-              </a>
-            </div>
-            <div className="user btn btn-ghost normal-case text-xl">
-              
-              <a className="" onClick={() => logOut()}>
-                LogOut
-              </a>
-            </div>
+            <a>Home</a>
           </div>
-        </aside>
-      </div>
+          <div
+            onClick={() => navigate("/admin/brand")}
+            className="brand btn btn-ghost normal-case text-xl"
+          >
+            <span class="material-symbols-sharp">bolt</span>
+            <a>Brand</a>
+          </div>
+          <div
+            onClick={() => navigate("/admin/category")}
+            className="category btn btn-ghost normal-case text-xl"
+          >
+            <span class="material-symbols-sharp">category</span>{" "}
+            <a className="">Category</a>
+          </div>
+          <div
+            onClick={() => navigate("/admin/car")}
+            className="car btn btn-ghost normal-case text-xl"
+          >
+            <span class="material-symbols-sharp">directions_car</span>{" "}
+            <a>Car</a>
+          </div>
+          <div
+            onClick={() => navigate("/admin/sale")}
+            className="sales btn btn-ghost normal-case text-xl"
+          >
+            <span class="material-symbols-sharp">sell</span>{" "}
+            <a className="">Sale</a>
+          </div>
+          <div
+            onClick={() => navigate("/admin/user")}
+            className="user btn btn-ghost normal-case text-xl"
+          >
+            <span class="material-symbols-sharp">person</span>{" "}
+            <a className="">User</a>
+          </div>
+          <div  onClick={() => logOut()} className="user btn btn-ghost normal-case text-xl">
+            <a >
+              LogOut
+            </a>
+          </div>
+        </div>
+      </aside>
+    </div>
   );
 };
 
