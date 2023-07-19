282ef270e6e2cdd04a0fad2293a38c9e70717fe0
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-18 22:32:07 -0400

    reviews

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,11 +1,12 @@
 import style from "./Card.module.css";
 import { Link } from "react-router-dom";
 import cart from "../../img/cart.png";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 
 const Card = (props) => {
   const [isAddedToCart, setIsAddedToCart] = useState(false);
+  const reviews = [1, 2, 3, 4, 5];
 
   const showPopup = () => {
     Swal.fire({
@@ -49,10 +50,29 @@ const Card = (props) => {
         <h3>{props.name} </h3>
         <p className={style.p}>${props.price}</p>
         <p className={style.p}>{props.age}</p>
+        
+        {
+          props.mediaReviews > 0 && <div className="rating">
+          {reviews.map((elem) => {
+            return (
+              <input
+                key={props.id}
+                type="radio"
+                name={`rating-${props.id}`} // Asignar un nombre único basado en el ID del elemento
+                className="mask mask-star-2 bg-orange-400"
+                defaultChecked={elem <= props.mediaReviews}
+                disabled // Marcar solo el elemento con índice 1 como seleccionado (puedes adaptar esto según tus necesidades)
+              />
+            );
+          })}
+        </div>
+        }
         <p className={style.celler}>{props.category.name}</p>
+        
       </div>
+
       <Link className={style.Link} to={`/detail/${props.id}`}>
-        <button className={style.button}>Check availability</button>
+        <button className={style.button}>View Details</button>
       </Link>
       <div className={style.cart} onClick={handleAddToCart}>
         <img src={cart} alt="" />
--- client/src/components/home/cardHome/CardHome.jsx
+++ client/src/components/home/cardHome/CardHome.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import "./CardHome.css";
 const CardHome = (props) => {
+  const reviews = [1, 2, 3, 4, 5];
   return (
     <>
       <Link className="Link" to={`/detail/${props.id}`}>
@@ -8,9 +9,28 @@ const CardHome = (props) => {
           <img className="imageCointainer" src={props.image} alt="" />
           <div>
             <p>{props.status}</p>
+
             <h2>{props.name} </h2>
+            {props.mediaReviews > 0 && (
+              <div className="rating p-3">
+                {reviews.map((elem) => {
+                  return (
+                    <input
+                      key={elem}
+                      type="radio"
+                      name={`rating-${props.id}`} // Asignar un nombre único basado en el ID del elemento
+                      className="mask mask-star-2 bg-orange-400"
+                      defaultChecked={elem <= props.mediaReviews}
+                      disabled // Marcar solo el elemento con índice 1 como seleccionado (puedes adaptar esto según tus necesidades)
+                    />
+                  );
+                })}
+              </div>
+            )}
             <h3 className="p">${props.price}</h3>
+
             <p className="brand">{props.brand.name}</p>
+           
           </div>
         </div>
       </Link>
