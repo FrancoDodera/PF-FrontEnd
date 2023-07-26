87a97e56119dfc26e9b62a26c3dc8928870ede89
FrancoDodera <francododera@hotmail.com>
2023-07-25 21:57:11 -0300

    favoriteEstilos

--- client/src/components/CardFavorite/CardFavorite.jsx
+++ client/src/components/CardFavorite/CardFavorite.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { removeFav } from "../../redux/actions";
 import likeimg from "../../img/like.jpeg";
 import noLikeimg from "../../img/nolike.png";
+import cart from "../../img/cart.png";
 
 const CardFavorites = ({
   id,
@@ -31,6 +32,28 @@ const CardFavorites = ({
     }
   };
 
+  const handleAddToCart = () => {
+    const item = {
+      id: id,
+      amount: 1,
+      name: name,
+      price: price,
+      totalPrice: price,
+      image: image,
+    };
+    const cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+    const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
+    if (existingItem) {
+      existingItem.amount += 1;
+      existingItem.totalPrice = existingItem.price * existingItem.amount;
+    } else {
+      cartItems.push(item);
+    }
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    setIsAddedToCart(true);
+    showPopup();
+  };
+
   useEffect(() => {
     favorites.forEach((fav) => {
       if (fav.id_car._id == id) {
@@ -40,33 +63,41 @@ const CardFavorites = ({
   }, [favorites]);
   return (
     <div className={Style.container1}>
-      {fav ? (
+      <div className={Style.iconsContainer}>
         <img
-          onClick={handleFavorite}
-          className={Style.likeImg}
-          src={likeimg}
-          alt={likeimg}
+          className={Style.cart}
+          onClick={handleAddToCart}
+          src={cart}
+          alt=""
         />
-      ) : (
-        <img
-          onClick={handleFavorite}
-          className={Style.likeImg}
-          src={noLikeimg}
-          alt={noLikeimg}
-        />
-      )}
+        {fav ? (
+          <img
+            onClick={handleFavorite}
+            className={Style.likeImg}
+            src={likeimg}
+            alt={likeimg}
+          />
+        ) : (
+          <img
+            onClick={handleFavorite}
+            className={Style.likeImg}
+            src={noLikeimg}
+            alt={noLikeimg}
+          />
+        )}
+      </div>
       <div>
-        <h1 className={Style.h1}>{name}</h1>
-        <img className={Style.image} src={image} alt="" />
+        <Link className={Style.Link} to={`/detail/${id}`}>
+          <h1 className={Style.h1}>{name}</h1>
+        </Link>
+        <Link className={Style.Link} to={`/detail/${id}`}>
+          <img className={Style.image} src={image} alt="" />
+        </Link>
         <p className={Style.p}>${price}</p>
         <p className={Style.p}>{age}</p>
         <p className={Style.p}>{status}</p>
         <p className={Style.p}>{category}</p>
         <p className={Style.p}>{brand}</p>
-        {/* <p className={Style.p}>{description}</p> */}
-        <Link className={Style.Link} to={`/detail/${id}`}>
-          <button className={Style.button}>View Details</button>
-        </Link>
       </div>
     </div>
   );
