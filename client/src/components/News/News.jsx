4ef633ee99abffb9a455c98cde848a5b552136c5
Alfredo Parada <104627681+AlfredoPA22@users.noreply.github.com>
2023-07-27 00:28:57 -0400

    arreglando cosas

--- client/src/components/News/News.jsx
+++ client/src/components/News/News.jsx
@@ -16,7 +16,7 @@ const News = () => {
     const apiKey = "84432522b9bf4608bfb735a7732a2ae3";
     const keywords = ["cars", "vehicles", "car reviews"];
     const keywordString = keywords.join(" ");
-    const url = `https://newsapi.org/v2/everything?q=${keywordString}&apiKey=${apiKey}&pageSize=${pageSize}&page=${currentPage}`;
+    const url = `https://newsapi.org/v2/everything?q=${keywordString}&apiKey=${apiKey}&pageSize=${pageSize}&page=1`;
 
     axios
       .get(url)
